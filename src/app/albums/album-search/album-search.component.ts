9fd0bea5d165baaacee7de2052de265fee94767b
Jorciney <jorciney.dias@hotmail.com>
2024-02-23 12:54:35 +0100

    M2 rxMethod

--- src/app/albums/album-search/album-search.component.ts
+++ src/app/albums/album-search/album-search.component.ts
@@ -7,8 +7,10 @@ import {AlbumListComponent} from './album-list/album-list.component';
 import {patchState, signalState} from "@ngrx/signals";
 import {AlbumState} from "@/albums/types";
 import {AlbumsService} from "@/albums/albums.service";
-import {take} from "rxjs";
+import {exhaustMap, pipe, take, tap} from "rxjs";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {rxMethod} from "@ngrx/signals/rxjs-interop";
+import {tapResponse} from "@ngrx/operators";
 
 @Component({
   selector: 'ngrx-album-search',
@@ -43,17 +45,21 @@ export default class AlbumSearchComponent implements OnInit {
   readonly totalAlbums = computed(() => this.filteredAlbums().length);
   readonly showSpinner = computed(() => this.albumSearchState.showProgress() && this.albumSearchState.albums().length === 0);
   readonly albumsService = inject(AlbumsService);
+  readonly loadAllAlbums = rxMethod<void>(pipe(
+    take(1),
+    tap(() => patchState(this.albumSearchState, {showProgress: true})),
+    exhaustMap(() => this.albumsService.getAll()),
+    tapResponse( // this is a custom operator that keeps the subscription alive when there's an error
+      (albums) => patchState(this.albumSearchState, {albums, showProgress: false}),
+      error => {
+        patchState(this.albumSearchState, {showProgress: false});
+        inject(MatSnackBar).open('Failed to load albums', 'Dismiss', {duration: 3000});
+      }
+    ),
+  ));
 
   ngOnInit(): void {
-    patchState(this.albumSearchState, {showProgress: true});
-    this.albumsService.getAll().pipe(take(1)).subscribe({
-        next: albums => patchState(this.albumSearchState, {albums, showProgress: false}),
-        error: () => {
-          patchState(this.albumSearchState, {showProgress: false});
-          inject(MatSnackBar).open('Failed to load albums', 'Dismiss', {duration: 3000});
-        }
-      }
-    )
+    this.loadAllAlbums();
   }
 
   updateQuery(query: string): void {
