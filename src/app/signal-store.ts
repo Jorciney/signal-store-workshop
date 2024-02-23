14e18abe14992210b7aee4074500505ce53978b9
Jorciney <jorciney.dias@hotmail.com>
2024-02-23 14:30:11 +0100

    M3 Signal Store

--- src/app/signal-store.ts
+++ src/app/signal-store.ts
@@ -1,5 +1,14 @@
-import {effect} from "@angular/core";
-import {PartialStateUpdater, patchState, signalState} from "@ngrx/signals";
+import {Component, computed, effect, inject, Injectable, OnInit} from "@angular/core";
+import {
+  PartialStateUpdater,
+  patchState,
+  signalState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState
+} from "@ngrx/signals";
 
 type User = { firstName: string, lastName: string };
 type UserState = { user: User, isAdmin: boolean };
@@ -34,3 +43,46 @@ const updateFirstName = (name: string): PartialStateUpdater<{ user: User }> => (
   }
 });
 patchState(userState, updateIsAdmin, updateFirstName('Name'));
+
+
+/// SIGNAL STORE
+
+type Todo = { id: number, text: string, completed: boolean };
+type TodoState = { todos: Todo[] };
+export const TodosStore = signalStore(
+  // {providedIn: 'root'}, // if you pass this, it will be a singleton for the whole app and not just the component
+  withState<TodoState>({todos: []}),
+  withComputed(({todos}) =>
+    ({completedTodos: computed(() => todos().filter(todo => todo.completed))})
+  ),
+  withMethods((store) => ({ //here you can also inject dependencies as it will be in an injection context
+    addTodo: (todo: Todo) => patchState(store, {todos: [...store.todos(), todo]})
+  })),
+  withHooks((store) => ({ //this will also be used in an injection context
+      onInit() {
+        console.log('this will be used onInit of the component initialized');
+      },
+      onDestroy() {
+        console.log('this will be used onDestroy of the component destroyed');
+      }
+    })
+  ));
+
+
+const initialState: TodoState ={
+  todos:[],
+}
+@Injectable
+class MyTodoStore extends signalStore(withState(initialState)) implements OnInit{
+  ngOnInit() {
+    console.log(this.todos()); // todos is available because we are extending the store
+  }
+}
+
+@Component({selector: '',
+  template: ``,
+  standalone: true,
+  providers: [TodosStore]})
+export class TodoComponent {
+  todoStore = inject(TodosStore);
+}
